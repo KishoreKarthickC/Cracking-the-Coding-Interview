9d331a1a7b96a7be04961133cfe9085927978daf
kishore <kchand23@asu.edu>
2017-12-30 01:41:09 -0700

    LinkedList

--- LinkedList/LinkedListDemo.java
+++ LinkedList/LinkedListDemo.java
@@ -32,14 +32,132 @@ public class LinkedListDemo {
 			curr = curr.next;
 		}
 	}
+	
+	public static void appendAtLast(int data)
+	{
+		Node temp1= new Node(data);
+		if(head == null)
+		{
+			head = temp1;
+			return;
+		}
+		Node curr = head;
+		while(curr.next!= null)
+		{
+			curr = curr.next;
+		}
+		curr.next = temp1;
+		return;
+	}
+	public static void midpointOfLinkedList(Node head)
+	{
+		Node slow_pointer = head;
+		Node fast_pointer = head;
+		
+		while(fast_pointer != null && fast_pointer.next != null)
+		{
+			slow_pointer = slow_pointer.next;
+			fast_pointer = fast_pointer.next.next;
+		}
+		System.out.println("the mid point is " + slow_pointer.data);
+	}
+	
+	public static void insertNodeAtGivenPosition(int n, int data)
+	{
+		Node temp =  new Node(data);
+		if(n ==1 )
+		{
+			temp.next = head.next;
+			head = temp;
+		}
+		
+		Node curr = head;
+        int i=0;
+		while(i < n-2)
+		{
+			curr = curr.next;
+			i++;
+		}
+		temp.next = curr.next;
+		curr.next = temp;
+		return;
+	}
+	
+	public static void deleteNodeAtGivenPosition(int n)
+	{
+		Node curr = head;
+		if(n==1)
+		{
+			head = curr.next;
+		}
+		
+	    for(int i=0; i < n-2; i++)
+	    {
+	    	curr =  curr.next;
+	    }
+	    curr.next = curr.next.next;	    		
+	}
+	
+	public static int isElementPresent(Node head, int x, int count)
+	{
+		Node curr = head;
+		if(curr == null)
+		{
+			return -1;
+		}
+		if(curr.data == x)
+		{
+			return count;
+		}
+		System.out.println("count is " + count);
+		return isElementPresent(curr.next, x, count+1);
+	}
+	
+	public static int lengthOfLinkedListRecursive(Node curr)
+	{
+		if(curr == null)
+		{
+			return 0;
+		}
+		
+		return 1+lengthOfLinkedListRecursive(curr.next);
+		
+	}
+	
+	public static void makeMiddleNodeAsHead(Node head)
+	{
+		Node curr = head;
+		Node slow_ptr = head;
+		Node fast_ptr = head;
+		Node prev_node = slow_ptr;
+		while(fast_ptr != null && fast_ptr.next != null)
+		{
+			prev_node = slow_ptr;
+			slow_ptr = slow_ptr.next;
+			fast_ptr = fast_ptr.next.next;
+		}
+		prev_node.next = null;
+		head = slow_ptr;
+	}
 	public static void main(String[] args)
 	{
 		for(int i=0; i < 10; i++)
 		{
 			insertAtFront(i);
 		}
+		appendAtLast(100);
 		print(head);
-		
+		midpointOfLinkedList(head);
+		insertNodeAtGivenPosition(4,100000000);
+		print(head);
+	    deleteNodeAtGivenPosition(1);
+	    System.out.println("The deleted list is ");
+	    print(head);
+	    System.out.println("The index of 2 is " + isElementPresent(head, 2, 0));
+	    System.out.println("The length of linked list is " + lengthOfLinkedListRecursive(head));
+	    makeMiddleNodeAsHead(head);
+	    System.out.println("The new list is ");
+	    print(head);
 	}
 
 }
